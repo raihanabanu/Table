3377bb4ad876b0c292f44722f4e756232ee7cf79
raihanabanu <raihanabanu.s@gmail.com>
2016-07-20 14:29:43 +0530

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,17 @@
+# Table
+import java.util.Scanner;
+public class Table {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int j=s.nextInt();
+	for(int n=0;n<=16;n++){
+		{
+		int  m=j*n;
+		System.out.println(j+"*"+n+"="+m);
+		}
+	}
+		
+	
+	}
+
+}
